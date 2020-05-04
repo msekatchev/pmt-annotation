ab35c2d45a981e687231c1d675df1b42b7e5d0e8
Dan <60349515+martindgr@users.noreply.github.com>
2020-05-04 12:39:31 +0100

    Uploaded annotate.py and README.txt

---
+++ README.txt
@@ -0,0 +1,27 @@
+annotate.py has two functions:
+
+annotate_img(img_path, size1, size2)
+    takes a path to an image (png is best) and two brush sizes for drawing a label on an image, calling the function opens the image to be drawn on with a mouse, and inputs taken are:
+    
+    size1 -> left mouse
+    size2 -> middle mouse
+    save image -> s
+    
+    This will save a 2D array as a png in the same directory as annotate.py code. It is currently set up to make a binary array (background=0, label=1). In order to make the label visible set line 59 to 255, (mask[mask != 0 ] = 255).
+    
+annotate_dir(img_dir, dataset, subset, size1, size2)
+    Takes a directory of images as input, requiring a specific directory structure that I use but shouldnt be too hard to alter if needed:
+    image_dir -> dataset -> subset_frames -> subset (images taken from here)
+                         -> subset_masks -> subset  (labels saved here)
+    
+    Inputs are:
+    
+    size1 -> left mouse
+    size2 -> middle mouse
+    save image -> s
+    skip and delete image -> a
+    
+the draw_circle function can be edited to change mouse bindings if necessary, unfortunately right mouse didnt work as drawing button for me hence the need for moddle mouse as the second size.
+In order to make the label visible set line 109 to 255.
+
+For IDBottomSurvey images I use size1:   (150 for whole pmt, 7 bolt, 35 dynode ), size2: (50 for whole, 5 bolt, 10 dynode)
---
+++ annotate.py
@@ -0,0 +1,118 @@
+import cv2
+import os
+import numpy as np
+
+
+#Set up callbacks for drawing circles on click and drag, bound to left and middle mouse 
+def draw_circle(event,x,y,flags,param):
+    global mouseX,mouseY
+    global ix, iy, drawing, rdrawing, mode
+    if event == cv2.EVENT_LBUTTONDOWN:
+        drawing = True
+        ix,iy = x,y
+        cv2.circle(img, (x, y), large_size,(255,0,0),-1)
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if drawing == True:
+                cv2.circle(img, (x, y), large_size,(255,0,0),-1)
+    elif event == cv2.EVENT_LBUTTONUP:
+        drawing = False
+
+    if event == cv2.EVENT_MBUTTONDOWN:
+        rdrawing = True
+        ix,iy = x,y
+        cv2.circle(img, (x, y), small_size,(255,0,0),-1)
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if rdrawing == True:
+                cv2.circle(img, (x, y), small_size,(255,0,0),-1)
+    elif event == cv2.EVENT_MBUTTONUP:
+        rdrawing = False
+
+        
+#Set up for a single image 
+def annotate_img(img_path, size1, size2) :
+    #Create window and put it in top left corner off screen
+    cv2.namedWindow('image')
+    cv2.moveWindow('image', 40, 30)
+    global drawing, rdrawing, large_size, small_size, img
+    large_size=size1
+    small_size=size2
+    
+    img = cv2.imread(img_path)
+    cv2.setMouseCallback('image',draw_circle)
+    
+    #Drawing and keyboard callbacks a to skip and delete, s to save image
+    drawing=False
+    rdrawing=False
+    while(1):
+        cv2.imshow('image',img)
+        k = cv2.waitKey(20) & 0xFF
+        if k == ord('s'):
+            #cv2.destroyWindow('image')
+            break
+
+    #Make mask same colour as drawing and output binarised image
+    train_labels = img[:,:,:3]
+    lower = np.array([254,0,0], dtype = "uint16")
+    upper = np.array([255,0,0], dtype = "uint16")
+    mask = cv2.inRange(train_labels, lower, upper)
+    mask[mask < 250] = 0
+    mask[mask != 0 ] = 1
+    
+    #save label in code directory
+    cv2.imwrite('label.png', mask )
+    cv2.destroyWindow('image')
+    return
+            
+
+#Set up for directory of images with file structure for image segmentation
+def annotate_dir(img_dir, dataset, subset, size1, size2) :
+    #Create window and put it in top left corner off screen
+    cv2.namedWindow('image')
+    cv2.moveWindow('image', 40, 30)
+    global drawing, rdrawing, large_size, small_size, img
+    large_size=size1
+    small_size=size2
+
+    
+    cv2.setMouseCallback('image',draw_circle)
+    #Array of names in directory to iterate over
+    f = []
+    for (dirpath, dirnames, filenames) in os.walk(f'{img_dir}{dataset}/{subset}_frames/{subset}'):
+        f.extend(filenames)
+        break
+    
+    print(f)
+    
+    for i in f :
+        skip = False
+        drawing = False
+        rdrawing = False
+        img = cv2.imread(f'{img_dir}{dataset}/{subset}_frames/{subset}/{str(i)}')
+        
+        #Drawing and keyboard callbacks a to skip and delete, s to save image
+        while(1):
+            cv2.imshow('image',img)
+            k = cv2.waitKey(20) & 0xFF
+            if k == ord('s'):
+                #cv2.destroyWindow('image')
+                break
+            elif k == ord('a'):
+                skip = True
+                #cv2.destroyWindow('image')
+                break
+        #Make mask same colour as drawing and output binarised image
+        train_labels = img[:,:,:3]
+        lower = np.array([254,0,0], dtype = "uint16")
+        upper = np.array([255,0,0], dtype = "uint16")
+        mask = cv2.inRange(train_labels, lower, upper)
+        mask[mask < 250] = 0
+        mask[mask != 0 ] = 1
+        
+        #Save label or delete image
+        if skip == False :
+            cv2.imwrite(f'{img_dir}{dataset}/{subset}_masks/{subset}/{str(i)}', train_labels )
+        else :
+            os.remove(f'{img_dir}{dataset}/{subset}_frames/{subset}/{str(i)}')
+    cv2.destroyWindow('image')
