1a9d894222168d6eb6ea66d12642224056eef797
Michael Sekatchev <64549194+msekatchev@users.noreply.github.com>
2020-05-11 15:32:20 -0700

    annotate_img now saves a text file with PMT feature coordinates
    - Worked on annotate_img function (will work on extending modifications to the annotate_dir function).
    - The function now creates a text file. Each line contains a PMT feature ID and its pixel coordinates on the image. The text file has the same name as the image.
    
    see README.txt for instructions.

--- annotate.py
+++ annotate.py
@@ -5,49 +5,40 @@ import numpy as np
 
 #Set up callbacks for drawing circles on click and drag, bound to left and middle mouse 
 def draw_circle(event,x,y,flags,param):
-    global mouseX,mouseY
-    global ix, iy, drawing, rdrawing, mode
-    global count
+    if(inputting==False):
+        global mouseX,mouseY
+        global ix, iy, drawing, rdrawing, mode
+        global count
+        
+        if event == cv2.EVENT_LBUTTONDOWN:
+            drawing = True
+            ix,iy = x,y
+            cv2.circle(img, (x, y), large_size,(255,0,0),-1)
+            print(ix, "x  ", iy,"y")
+            ##pmtID = input("Input PMT feature ID (or d to delete): ")
+
+    #    elif event == cv2.EVENT_MOUSEMOVE:
+    #        if drawing == True:
+    #                cv2.circle(img, (x, y), large_size,(255,0,0),-1)
+        elif event == cv2.EVENT_LBUTTONUP:
+            drawing = False
 
 
-    
-    if event == cv2.EVENT_LBUTTONDOWN:
-        drawing = True
-        ix,iy = x,y
-        cv2.circle(img, (x, y), large_size,(255,0,0),-1)
+        if event == cv2.EVENT_MBUTTONDOWN:
+            rdrawing = True
+            ix,iy = x,y
+            cv2.circle(img, (x, y), small_size,(255,0,0),-1)
+            print(ix, "x  ", iy,"y")
 
-    elif event == cv2.EVENT_MOUSEMOVE:
-        if drawing == True:
-                cv2.circle(img, (x, y), large_size,(255,0,0),-1)
-    elif event == cv2.EVENT_LBUTTONUP:
-        drawing = False
-
-
-
-    if event == cv2.EVENT_MBUTTONDOWN:
-        rdrawing = True
-        ix,iy = x,y
-        cv2.circle(img, (x, y), small_size,(255,0,0),-1)
-
-    elif event == cv2.EVENT_MOUSEMOVE:
-        if rdrawing == True:
-                cv2.circle(img, (x, y), small_size,(255,0,0),-1)
-    elif event == cv2.EVENT_MBUTTONUP:
-        rdrawing = False
-   
-
-    
-#    coords[count][0] = ix
-#    coords[count][1] = iy
-    if(rdrawing==True or drawing==True ):
-        coords[0].append(ix)
-        coords[1].append(iy)
-        print("PX: ", ix, "  ", iy)
-      #  print("XY: ",coords[0][count], "  ",coords[1][count])
-        print("---------------", coords)
-        count = count+1
+    #    elif event == cv2.EVENT_MOUSEMOVE:
+    #        if rdrawing == True:
+    #                cv2.circle(img, (x, y), small_size,(255,0,0),-1)
+        elif event == cv2.EVENT_MBUTTONUP:
+            rdrawing = False
+       
 
 
+                
 
 
         
@@ -57,8 +48,19 @@ def draw_circle(event,x,y,flags,param):
         ##img_path - name and location of the image, example "B.jpg"
         ##size1 - size of the 1st brush circle, in px
         ##size2 - size of the 2nd brush circle, in px
+    ##outputs
+        ##label.jpg - A map of all the features in binary (white on black)
+        ##{image_name}.txt - A text file. Each line contains a PMT feature ID and its pixel coordinates on the image. The text file has the same name as the image.
+
+    ##User keypresses (make sure the image window is selected):
+        ## click on image to grab pixel coordinates
+        ## s to close image and exit program
+        ## r to write coordinates to file (you will be prompted for the PMT feature ID)
 def annotate_img(img_path, size1, size2) :
+    global recordCoords 
     global count
+    global inputting
+    inputting = False
     count = 0
     global coords
     coords = [[], []]
@@ -72,42 +74,66 @@ def annotate_img(img_path, size1, size2) :
     
     img = cv2.imread(img_path) ##read the image specified in the input
     cv2.setMouseCallback('image',draw_circle) ##Link mouse position and button states to the draw_circle function.
-    
-    #Drawing and keyboard callbacks a to skip and delete, s to save image
+
+    #Extract the name of the image from the inputted path to the image.
+    base = os.path.basename(img_path)
+    filename = os.path.splitext(base)[0]
+    file = open("%s.txt" %filename,"w")
+    print("Saving to: ",filename,".txt")
+
+    #Drawing callbacks
     drawing=False
     rdrawing=False
 
-#    while(1):
-#        cv2.imshow('image',img)     ##keep displaying the image even if the user exits.
-#        k = cv2.waitKey(20) & 0xFF
-#        if k == ord('s'):           ##if the s key is pressed, exit the loop.
-#            #cv2.destroyWindow('image')
-#            break
-
-    k = cv2.waitKey(20) & 0xFF
+    print("\n Click on image to grab pixel coordinates.\n Press s to close image and exit program.\n Press r to write coordinates to file. \n    (you will be prompted for the PMT feature ID)")
     
-    while(k!=ord('s')):
-        cv2.imshow('image',img)
-        k = cv2.waitKey(20) & 0xFF
-        
-        
 
+    while(1):
+        cv2.imshow('image',img)     ##keep displaying the image even if the user exits.
+        k = cv2.waitKey(20) & 0xFF
+        if(k == ord('s')):           ##if the s key is pressed, exit the loop.
+            #cv2.destroyWindow('image')
+            break
+
+        
+###############################Add line to text output###############################
+        if(k==ord('r')):
+            inputting = True ##used to pause the draw_circle function from recording more coordinates
+            print("Recording: Pixels: ", ix, "x  ", iy,"y")
+            pmtID = input("Input PMT feature ID to add it to the list, or input 'd' to not register: ")
+            if(pmtID != 'd'):
+                file.write("%s\t%d\t%d\n" %(pmtID, ix, iy))
+                coords[0].append(ix)
+                coords[1].append(iy)
+                #print("PX: ", ix, "  ", iy)
+                #print("XY: ",coords[0][count], "  ",coords[1][count])
+                #print(coords)
+                count = count+1 ##currently unused, but can be used to track number of entries in the file.
+                #recordCoors = False
+            inputting = False
+###############################Add line to text output###############################
+
+            
+    file.close
+###############################Image Output##########################################
     #Make mask same colour as drawing and output binarised image
     train_labels = img[:,:,:3]
-    print("TRAIN LABELS = ")
-    print(train_labels)
+    # print("TRAIN LABELS = ")
+    #print(train_labels)
     lower = np.array([254,0,0], dtype = "uint16")
     upper = np.array([255,0,0], dtype = "uint16")
     mask = cv2.inRange(train_labels, lower, upper)
     mask[mask < 250] = 0
     mask[mask != 0 ] = 255 ##set =1 for binary or =255 for visible white
-    
+
     #save label in code directory
     cv2.imwrite('label.png', mask )
-    print(mask)
+    #print(mask)
+###############################Image Output##########################################
+
     cv2.destroyWindow('image')
 
-    np.savetxt("array.txt", mask, fmt="%s")
+    np.savetxt("array.txt", coords, fmt="%s")
     return
             
 
